99ac58fb2fc243f9bd79e995911a736b2f468ab3
Omo Agbagbara <joomla@deltastateonline.com>
2013-08-28 06:29:42 +1000

    New Asset tab

---
+++ components/com_clubreg/assets/js/property.js
@@ -0,0 +1,121 @@
+window.addEvent('domready', function () {	
+	
+	var propertyTabDivs = new renderingDivs();
+	var propertyListDiv = new divListRenderer();
+	
+	
+	propertyListDiv.params = "option=com_clubreg&view=ajax&layout=propertys&tmpl=component";
+	
+	if($('propertyFormDiv')){		
+		propertyTabDivs.setObjects(new Fx.Morph($('propertyFormDiv')),1);
+		propertyTabDivs.setArray1($('propertyFormDiv').getStyle('margin-left'),10);	
+	}
+	if($('profile-propertys')){		
+		propertyTabDivs.setObjects(new Fx.Morph($('profile-propertys')),2);
+		propertyTabDivs.setArray2($('profile-propertys').getStyle('margin-left'),profilediverightedge);		
+		
+		propertyListDiv.setDivObject($('profile-propertys'));
+		propertyListDiv.renderList();
+	}	
+	
+	$$(".profile-property-button").addEvent('click',function(event){	
+		
+		if(propertyTabDivs.inOut){ 
+			$('propertyFormDiv').empty();
+			$('propertyFormDiv').addClass('loading1');
+			addProperty($(this));
+		}		
+		propertyTabDivs.toggle_div();		
+	});
+	
+	var container = $('propertyFormDiv');
+	
+	if(container){
+		
+		container.addEvent("submit:relay($('property-form'))", function(event){
+			
+			event.stop();
+			
+			$('loading-div').addClass('loading-small');		
+			
+			new Request({
+	            url: this.get("action"),
+	            data: this,
+	            onComplete: function() {
+	              
+	               var json_data = JSON.decode(this.response.text);	
+	               if(!json_data["proceed"]){            	
+	           			alert(json_data["msg"]);
+	               }
+	               if(json_data["isNew"]){
+	            	   $('profile-propertys').empty();
+	            	   $('profile-propertys').addClass('loading1');
+	            	   propertyListDiv.renderList();
+	            	   propertyTabDivs.toggle_div();	
+	               }else{
+	            	   propertyTabDivs.toggle_div();
+	            	   load_property(json_data["property_id"]);
+	               }
+	               $('loading-div').removeClass('loading-small');
+	            }
+	        }).send();			
+	
+		});	
+		
+		container.addEvent("click:relay($('toggle-propertys-div'))", function(event){			
+			if($(this).id == "toggle-propertys-div"){
+				propertyTabDivs.toggle_div();
+			}
+		});
+	
+		
+		 var container = $('profile-propertys'); // after an item has been added we should still able to edit it
+		 if(container){
+			 container.addEvent('click:relay(.profile-property-button)', function(){ // click event on link
+				 $('propertyFormDiv').empty();
+				 $('propertyFormDiv').addClass('loading1');
+				 addProperty($(this));
+				 propertyTabDivs.toggle_div();	
+			 });			 
+			
+		 }
+	
+	}
+	
+});
+
+function addProperty(dObject){
+	
+	var json_data = JSON.decode(dObject.get('rel'));		
+	var params = "option=com_clubreg&view=ajax&layout=property&tmpl=component";		
+	var durl = "index.php?"+params;	
+	
+	var a = new Request.HTML({
+		url : durl, 
+		method: 'post',	
+		data : json_data,
+		update: $('propertyFormDiv'),
+		onSuccess: function(responseText){ $('propertyFormDiv').removeClass('loading1');	$('loading-div').removeClass('loading-small');}
+		}).send();	
+	
+}
+
+function load_property(property_id){
+	var wDiv = $('propertydata_'+property_id);
+	
+	wDiv.empty();	
+	wDiv.addClass('loading1');
+	
+	var json_data = JSON.decode(wDiv.get('rel'));		
+	var params = "option=com_clubreg&view=ajax&layout=aproperty&tmpl=component&format=raw";		
+	var durl = "index.php?"+params;	
+	
+	var a = new Request.HTML({
+		url : durl, 
+		method: 'post',		
+		update: wDiv,
+		data : json_data,
+		onSuccess: function(responseText){ wDiv.removeClass('loading1')},
+		onFailure:function(){ profileFailure(this); }
+		}).send();
+}
---
+++ components/com_clubreg/views/regmember/tmpl/viewonly_property.php
@@ -0,0 +1,25 @@
+<?php
+/*------------------------------------------------------------------------
+# com_clubreg - Manage Club Member Registrations
+# ------------------------------------------------------------------------
+# author    Omokhoa Agbagbara
+# copyright Copyright (C) 2012 applications.deltastateonline.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://applications.deltastateonline.com
+# Technical Support:  email - joomla@deltastateonline.com  
+-------------------------------------------------------------------------*/ 
+// no direct access
+/**
+ * @todo add this to the language file COM_CLUBREG_PROFILE_PROPERTY
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
+$in_type = "hidden";
+global $clubreg_Itemid;
+$rel_string_list = array("Itemid"=>$clubreg_Itemid,"member_key"=>$this->member_key,JSession::getFormToken()=>1);
+$rel_string_edit = array("Itemid"=>$clubreg_Itemid,"member_key"=>$this->member_key,JSession::getFormToken()=>1,'property_key'=>'0');
+?>
+<div><a class="btn profile-property-button" href="javascript:void(0)" rel=<?php echo json_encode($rel_string_edit)?>>Add <?php echo JText::_('COM_CLUBREG_PROFILE_PROPERTY')?></a></div>
+<div class="clugreg-div-wrapper">
+	<div class="clugreg-div form-div" id="propertyFormDiv" rel=<?php echo json_encode($rel_string_edit)?> ></div>
+	<div class="clugreg-div loading1" id="profile-propertys" rel=<?php echo json_encode($rel_string_list)?> ></div>	
+</div>
