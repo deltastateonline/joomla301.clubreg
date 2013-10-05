ce62df43a700f3310c0ca513f41edcc9cd5b7d49
Omo Agbagbara <joomla@deltastateonline.com>
2013-10-05 11:56:02 +1000

    more fixes

--- components/com_clubreg/views/other/view.raw.php
+++ components/com_clubreg/views/other/view.raw.php
@@ -71,8 +71,8 @@ class ClubRegViewOther extends ClubRegViews
 			$currentModel->setState('com_clubreg.other.member_id',$key_data->pk_id); // use the key in the model
 			$currentModel->setState('com_clubreg.other.member_key',$key_data->full_key); // use the key in the model
 			
-			require_once CLUBREG_ADMINPATH.'/helpers/clubregControls.php';
-			require_once CLUBREG_ADMINPATH.'/helpers/clubregControlsReadonly.php';
+			require_once CLUBREG_ADMINPATH.'/helpers/clubregcontrols.php';
+			require_once CLUBREG_ADMINPATH.'/helpers/clubregcontrolsreadonly.php';
 			JForm::addFieldPath(CLUBREG_ADMINPATH.'/models/fields');
 			$this->extradetails = ClubRegControlsHelper::configOptions(CLUB_PLAYER_DETAILS); // controls				
 			
@@ -83,47 +83,5 @@ class ClubRegViewOther extends ClubRegViews
 	
 		return $proceed;
 	}
-	
-	protected function edit_other(){
-	
-		JSession::checkToken() or die(JText::_('JINVALID_TOKEN'));
-	
-		$this->setLayout("form.other");
-	
-		$user		= JFactory::getUser();
-		$app			= JFactory::getApplication();
-		$Itemid			= $app->input->post->get('Itemid');
-	
-		$proceed = FALSE;
-		if($user->get('id') > 0){
-	
-			require_once JPATH_COMPONENT.DS.'helpers'.DS.'clubreg.uniquekeys.php';
-	
-			$proceed = TRUE;
-			$key_data = new stdClass();
-	
-			$key_data->full_key = $app->input->post->getString('member_key', null);
-			$this->uKeyObject = new ClubRegUniqueKeysHelper();
-			$this->uKeyObject->deconstructKey($key_data);
-	
-			unset($currentModel);
-			$currentModel = JModelLegacy::getInstance('other', 'ClubregModel', array('ignore_request' => false));
-			$currentModel->setState('com_clubreg.other.member_id',$key_data->pk_id); // use the key in the model
-			$currentModel->setState('com_clubreg.other.member_key',$key_data->full_key); // use the key in the model
-	
-			require_once CLUBREG_ADMINPATH.'/helpers/clubregControls.php';
-			JForm::addFieldPath(CLUBREG_ADMINPATH.'/models/fields');
-			$this->extradetails = ClubRegControlsHelper::configOptions(CLUB_PLAYER_DETAILS); // controls
-	
-			$this->otherForm = $currentModel->getForm();
-			$this->otherValues = $currentModel->get('otherValues');
-	
-	
-		}
-	
-		return $proceed;
-	
-	}
-	
 
 }
