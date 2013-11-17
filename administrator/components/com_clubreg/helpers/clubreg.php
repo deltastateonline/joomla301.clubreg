2adfef3edbcae6436805a8765e93f63895402cdd
Omo Agbagbara <joomla@deltastateonline.com>
2013-11-18 06:10:26 +1000

    Bug fixes

--- administrator/components/com_clubreg/helpers/clubreg.php
+++ administrator/components/com_clubreg/helpers/clubreg.php
@@ -116,10 +116,11 @@ class ClubRegHelper
 			}
 			
 			$query->where('published = 1');
+			
 			if(isset($parent_data)){
-				$ordering = $parent_data->params["sort_list_by"];	
-				if(!is_null($ordering)){
-					$ordering = $ordering." ASC ";
+				$tordering = trim($parent_data->params["sort_list_by"]);	
+				if(!is_null($tordering) && strlen($tordering) > 0){
+					$ordering = $tordering." ASC ";
 				}			
 			}					
 			
