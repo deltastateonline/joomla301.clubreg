cbdd35870260b533284207b6c33808940c775b9d
Omo Agbagbara <joomla@deltastateonline.com>
2014-10-29 19:12:48 +1000

    Using jquery for comms

--- components/com_clubreg/assets/js/communications.js
+++ components/com_clubreg/assets/js/communications.js
@@ -55,11 +55,60 @@ jQuery( document ).ready(function() {
 	jQuery('#reg-filter-selected').html(all_selected);
 	
 	
-	jQuery(".comm-delete-message").on('click',function(event){		
-
-		//alert("what now");
-		jQuery(this).parents('div.comms-div').fadeOut();
+	jQuery(document).on('click',".btn-delete-comm",function(event){		
+		
+		event.stopPropagation();
+		event.preventDefault();			
+		
+		var post_data = jQuery('#comm-delete-form').serialize();		
+		
+		jQuery.post("index.php",post_data,function(data){
+			
+			if(data.msg_content){
+				jQuery('#comm_action_msg').html(data.msg_content);	
+				jQuery('#comm_action_btn').html(data.msg_btn);	
+			}
+			
+			if(data.proceed && data.comm_id){				
+				jQuery(".comms-div").filter("[data-comm_id=\""+data.comm_id+"\"]").fadeOut();					
+			}			
+			
+		},'json').fail(function(){
+			alert("Unable to process request!");
+		});
+		
 	});	
+	
+	
+	jQuery(document).on('click',".comm-reset-msg",function(event){	
+		
+		event.stopPropagation();
+		event.preventDefault();		
+		
+		var comm_id = jQuery(this).parents('.comms-div').data('comm_id');
+		
+		//jQuery(this).attr('href');
+		var url = jQuery(this).attr('href');
+		
+		post_data = {};
+		
+		jQuery.get(url,function(data){			
+			
+			if(data.proceed && data.comm_id){				
+				jQuery(".comms-div").filter("[data-comm_id=\""+data.comm_id+"\"]").fadeOut();					
+			}else{
+				alert(data.msg_content);
+			}	
+			
+		},'json').fail(function(){
+			alert("Unable to process request!");
+		});
+		
+		
+		
+	});
+	
+	
 	
 	
 });
