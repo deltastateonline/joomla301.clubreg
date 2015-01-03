c2bcd8e840b358fc58473f69f4fcdffcc71be6dc
Omo Agbagbara <joomla@deltastateonline.com>
2015-01-03 11:40:28 +1000

    Refactor profile page

--- components/com_clubreg/views/regmember/tmpl/viewonly_newprofile.php
+++ components/com_clubreg/views/regmember/tmpl/viewonly_newprofile.php
@@ -35,27 +35,22 @@ $headingConfigs = $this->profileConfigs["headings"];
 	  $member_data->t_phone = $t_phone ;
 	  /** some preprocessing*/	  
 
-	 if(isset($this->profileConfigs["headings"]["club"])){ ?>		
-		<div class="row-fluid frame-div" style="max-width:97%">
-			
-			<div class="h21"><?php echo JText::_('COM_CLUBREG_PROFILE_DIVISION'); ?> :&nbsp;&nbsp;
-				<a class="btn btn-mini profile-div-button pull-right" rel='<?php echo $member_data->member_id; ?>' href="javascript:void(0);">-</a><br />
-			 	
-			 </div>	
-			<div class="row-striped">
-			<div id="profile-division">
-				<?php $profile_details =  $headingConfigs["club"]; 
-					foreach($profile_details as $akey => $tvalue){ 					
-					?>
-					<div class="row-fluid">
-						<div class="pull-left profile-label"><?php echo $tvalue["label"]?></div>
-						<div class="pull-left profile-colon"> :&nbsp;&nbsp;</div>
-						<div class="pull-left profile-value"><?php echo $itemRenderer->render($member_data->$akey,$tvalue); ?></div>
-					</div>
-				<?php }?>
-				</div>
-			</div>	
-		</div>
-		<?php }else{ ?>
-			No Details
-		<?php } ?>
+	  
+?>
+	<div class="row-fluid" style="max-width:97%;">
+	<div class="span3">&nbsp;</div>
+	<div class="span3"><?php echo $this->loadTemplate("pixs"); ?></div>
+	<div class="frame-div span6">
+			<div class="h21"><?php echo JText::_('COM_CLUBREG_PROFILE_BIO'); ?> : </div>
+					<?php $profile_details = $headingConfigs["bio"]; 
+						foreach($profile_details as $akey => $tvalue){ 					
+						?>
+						<div class="row-fluid" style="border-bottom:1px solid #EEEFEF">
+							<div class="pull-left profile-label1"><?php echo $tvalue["label"]?></div>
+							<div class="pull-left" style='padding:0px 5px;'> : </div>
+							<div class="pull-left profile-value"> <?php echo $itemRenderer->render($member_data->$akey,$tvalue); ?></div>
+						</div>
+					<?php }?>		
+		</div>	
+		
+	</div>		
