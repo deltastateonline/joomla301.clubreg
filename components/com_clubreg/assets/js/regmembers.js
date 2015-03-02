4e8be21b1f6f74e73095517c0bd6a461afaff785
Omo Agbagbara <joomla@deltastateonline.com>
2015-03-03 06:25:25 +1000

    move logic for filters

--- components/com_clubreg/assets/js/regmembers.js
+++ components/com_clubreg/assets/js/regmembers.js
@@ -2,7 +2,7 @@ window.addEvent('domready', function () {
 	
 
 	$$('.reg-button-all').addEvent('click',function(event){	
-		
+		return;
 		if(this.get('html') == "+"){ ticon = "-";	}else{ ticon = "+";	}
 		this.set('html',ticon);
 		
@@ -89,52 +89,6 @@ window.addEvent('domready', function () {
 			group_onchange($("batch_group"),$("batch_subgroup"),-1);
 		});
 	}
-
-
-	
-	if($('playertype')){
-		$('playertype').addEvent('change', function (){	
-			var all_filters =  $('adminForm').getElements('input[type=text]');			
-			all_filters.each(function(a_filter){	
-				a_filter.value = ""; 
-			});
-			
-			var all_filters =  $('adminForm').getElements('select');			
-			all_filters.each(function(a_filter){
-				if(a_filter.name == "playertype" || a_filter.name == "member_status" ){
-					
-				}else{
-					a_filter.value = "-1";
-				}
-			});
-		});
-	}
-	
-	var all_selected = "";
-	var empty_str = /^\s*$/;
-	
-	var all_filters =  $('adminForm').getElements('input[type=text]');			
-	all_filters.each(function(a_filter){		
-			var s_item = a_filter.value;
-			if(s_item && ! empty_str.test(s_item)){
-				all_selected = all_selected + "<span class='label label-info '>"+a_filter.value+"</span><span class='divider'>&nbsp;</span>";
-			}
-	});
-	
-	var all_filters =  $('adminForm').getElements('select');
-	var ignore_these = ["playertype","directionTable","sortTable"];
-	all_filters.each(function(a_filter){	
-		
-		var s_item = a_filter.getSelected();
-		
-		if(a_filter.value == -1 || (ignore_these.indexOf(a_filter.name) > -1)){
-			
-		}else{			
-			all_selected = all_selected +  "<span class='label label-info'>"+s_item[0].get('text')+"</span> <span class='divider'>&nbsp;</span>";			
-		}
-		
-	});	
-	$('reg-filter-selected').set('html',all_selected);
 	
 });
 
