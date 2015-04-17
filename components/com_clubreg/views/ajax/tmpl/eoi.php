d33ada2848486fcc90ce646687f6534ec85ffc56
Omo Agbagbara <joomla@deltastateonline.com>
2015-04-18 07:48:27 +1000

    restyle eois count

--- components/com_clubreg/views/ajax/tmpl/eoi.php
+++ components/com_clubreg/views/ajax/tmpl/eoi.php
@@ -11,13 +11,15 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 global $clubreg_Itemid;
-
-
-
 $d_url = sprintf("index.php?option=com_clubreg&view=eois&layout=rendereoi&Itemid=%s&playertype=",
 		$clubreg_Itemid	);
 ?>
-<ul>
-	<li><a href="<?php echo $d_url ;?>guardian"><?php echo JText::_('CLUBREG_DASH_GUARDIAN'); ?> (<?php echo $this->howmany_guardian; ?>)</a></li>
-	<li><a href="<?php echo $d_url ;?>senior"><?php echo JText::_('CLUBREG_DASH_SENIOR');?> (<?php echo $this->howmany_senior; ?>)</a></li>
-</ul>
+<div class="eoi-counts-div">
+	<div class="eoi-counts pull-left"><div class='eoi-count-head'><a href="<?php echo $d_url ;?>guardian"><?php echo JText::_('CLUBREG_DASH_GUARDIAN'); ?></a></div>
+		<div class="eoi-count-body"><?php echo $this->howmany_guardian; ?></div>
+	</div>
+	<div class="eoi-counts pull-right"><div class='eoi-count-head'><a href="<?php echo $d_url ;?>senior"><?php echo JText::_('CLUBREG_DASH_SENIOR');?></a></div>
+		<div class="eoi-count-body"><?php echo $this->howmany_senior; ?></div>
+	</div>	
+	<div class="clearfix"></div>
+</div>
