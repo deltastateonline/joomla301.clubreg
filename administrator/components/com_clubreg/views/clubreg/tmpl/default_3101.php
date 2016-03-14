880d7bfd90350d38fa62e9055abb7ca42943daae
Omo Agbagbara <joomla@deltastateonline.com>
2016-03-14 19:38:54 +1000

    commerical release

--- administrator/components/com_clubreg/views/clubreg/tmpl/default_3101.php
+++ administrator/components/com_clubreg/views/clubreg/tmpl/default_3101.php
@@ -1,10 +1,11 @@
 <?php echo JHtml::_('bootstrap.addSlide', 'slide-updates', JText::_('This Update. 3.1.01 - Commercial Release 1'), 'version-3101'); ?>
-This update is a the first release of the commercial version of clubreg 3. It includes
+This update is a the first release of the commercial version of clubreg 3. This release doesn't contain any google ads. It also includes
 <ul>
 	<li>Attendance Reporting</li>
 	<li>Direct link for regular updates.</li>	
 	<li>Feature to create a contact list for each player who can also be notified of club activities.</li>
-	<li>Feature to upload a csv file.</li>	
+	<li>Feature to upload a csv file.</li>
+	<li>No Google Ads</li>	
 </ul>
 
 <?php echo JHtml::_('bootstrap.endSlide'); ?>
