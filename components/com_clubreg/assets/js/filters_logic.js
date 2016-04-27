8ca49f8869aa8510e9a465bfbe18e496ae42bb0a
Omo Agbagbara <joomla@deltastateonline.com>
2016-04-27 22:17:01 +1000

    Refactor code to use the jquery to show selected filters

--- components/com_clubreg/assets/js/filters_logic.js
+++ components/com_clubreg/assets/js/filters_logic.js
@@ -17,38 +17,47 @@ window.addEvent('domready', function () {
 				}
 			});
 		});
-	}
+	}	
+});
+
+jQuery(document).ready(function() {
 	
+	if(jQuery('#playertype')){
+		
+	}
 	
 	var all_selected = "";
 	var empty_str = /^\s*$/;
 	
-	var all_filters =  $('adminForm').getElements('input[type=text]');	
+	var all_filters =  jQuery('#adminForm input:text');	
 	var ignore_these = ["stats_date","end_date"];
-	all_filters.each(function(a_filter){		
-			var s_item = a_filter.value;
+	
+	all_filters.each(function(idx){		
+		
+		var a_filter = all_filters[idx];
+		var s_item = a_filter.value;
+		if((ignore_these.indexOf(a_filter.name) > -1)){
 			
-			if((ignore_these.indexOf(a_filter.name) > -1)){
-				
-			}else if(s_item && ! empty_str.test(s_item)){
-				all_selected = all_selected + "<span class='label label-info '>"+a_filter.value+"</span><span class='divider'>&nbsp;</span>";
-			}
+		}else if(s_item && ! empty_str.test(s_item)){
+			all_selected = all_selected + "<span class='label label-info '>"+a_filter.value+"</span><span class='divider'>&nbsp;</span>";
+		}
+		
 	});
 	
-	var all_filters =  $('adminForm').getElements('select');
+	var all_filters =  jQuery('#adminForm select');
+	
 	var ignore_these = ["playertype","directionTable","sortTable","limit"];
-	all_filters.each(function(a_filter){	
+	all_filters.each(function(idx){	
 		
-		var s_item = a_filter.getSelected();
+		var a_filter = all_filters[idx];		
 		
 		if(a_filter.value == -1 || (ignore_these.indexOf(a_filter.name) > -1)){
 			
-		}else{			console.log(s_item);
-			all_selected = all_selected +  "<span class='label label-info'>"+s_item[0].get('text')+"</span> <span class='divider'>&nbsp;</span>";			
-		}
-		
+		}else{	
+			var s_item = jQuery(a_filter).find(":selected").text()
+			all_selected = all_selected +  "<span class='label label-info'>"+s_item+"</span> <span class='divider'>&nbsp;</span>";			
+		}		
 	});	
-	$('reg-filter-selected').set('html',all_selected);
 	
-	
+	jQuery('#reg-filter-selected').html(all_selected);
 });
