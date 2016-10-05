6265c434f8f2d1016201845e9b8f0c7f519c59fe
Omo Agbagbara <joomla@deltastateonline.com>
2016-10-05 19:38:58 +1000

    Add new form to which edit request are sent

--- components/com_clubreg/views/stats/tmpl/expresscheckin.php
+++ components/com_clubreg/views/stats/tmpl/expresscheckin.php
@@ -26,16 +26,32 @@ $in_type = "hidden";
 <script type="text/javascript">
 <!--
 	var token = '<?php echo JSession::getFormToken() ;?>';
+
+	Joomla.sbutton = function(pk,playerType)
+	{
+		var form = document.getElementById('adminForm');		
+		document.adminForm.target='';
+		document.adminForm.playertype.value= playerType;
+		
+		document.adminForm.pk.value = pk;
+		form.submit();
+	}
+	
 //-->
 </script>
-<form action="<?php echo JRoute::_($this->formaction); ?>" method="post" name="adminForm" id="expresscheckin-adminForm" class="form-inline">
+<form action="<?php echo JRoute::_($this->formaction); ?>" method="post" name="expresscheckinAdminForm" id="expresscheckinAdminForm" class="form-inline">
 	
 	<div class="control-group" id="express-checkin-div">
-		<label class="control-label" for="<?php echo JText::_('COM_CLUBREG_PLAYERNAME_LABEL'); ?>"><?php echo JText::_('COM_CLUBREG_PLAYERNAME_LABEL'); ?></label> : 
+		<label class="control-label" for="search_value"><?php echo JText::_('COM_CLUBREG_PLAYERNAME_LABEL'); ?></label> : 
 		<div class="input-append" >		 	
-	  		<input type="text" placeholder="<?php echo JText::_('COM_CLUBREG_PLAYERNAME_LABEL'); ?>" class="inputbox input-xlarge"  required name="search_value" id="search_value" />
+	  		<input type="text" placeholder="<?php echo JText::_('COM_CLUBREG_PLAYERNAME_LABEL'); ?>" class="inputbox input-large"  required name="search_value" id="search_value" />
 	  		<a href="javascript:void(0);" class="btn"><span class="icon-search"></span ></span></a>
 	  	</div>
+	  	<?php  $attribs = array("class"=>"inputbox input-small","onchange"=>"expressCheckinDateChanged();"); ?>	  	
+	  	<?php  echo JHtml::calendar($this->stats_date, "stats_date", 'stats_date','%d/%m/%Y',$attribs) ?>
+  		
+	  	
+	  	
 	</div>
 	<input type="<?php echo $in_type; ?>" name="Itemid" value="<?php echo $clubreg_Itemid; ?>" />	
 	<input type="<?php echo $in_type; ?>" name="option" value="com_clubreg" />
@@ -43,7 +59,23 @@ $in_type = "hidden";
 	<input type="<?php echo $in_type; ?>" name="layout" value="<?php echo $this->layout; ?>" />
 	<input type="<?php echo $in_type; ?>" name="stats_date" id="stats_date" value="<?php echo $this->stats_date; ?>" />
 	<input type="<?php echo $in_type; ?>" name="view" value="stats" />
-	<input type="<?php echo $in_type; ?>" name="format" value="json" />
+	<input type="<?php echo $in_type; ?>" name="format" value="raw" />
+	
+	
+	<input type="<?php echo $in_type; ?>" name="boxchecked" value="0" />
+	<input type="<?php echo $in_type; ?>" name="filter_order" id="filter_order" value="<?php echo $this->escape($this->state->get('list.ordering')); ?>" />
+	<input type="<?php echo $in_type; ?>" name="filter_order_Dir" id="filter_order_Dir" value="<?php echo $this->escape($this->state->get('list.direction')); ?>" />
+	<?php echo JHTML::_( 'form.token' ); ?>
+</form>
+
+<form action="<?php echo JRoute::_($this->formaction_edit); ?>" method="post" name="adminForm" id="adminForm" class="form-inline">
+	
+	<input type="<?php echo $in_type; ?>" name="Itemid" value="<?php echo $clubreg_Itemid; ?>" />	
+	<input type="<?php echo $in_type; ?>" name="option" value="com_clubreg" />
+	
+	<input type="<?php echo $in_type; ?>" name="layout" value="viewonly" />
+	<input type="<?php echo $in_type; ?>" name="view" value="regmember" />	
+	<input type="<?php echo $in_type; ?>" name="playertype" value="" />
 	<input type="<?php echo $in_type; ?>" name="pk" value="" />
 	
 	<input type="<?php echo $in_type; ?>" name="boxchecked" value="0" />
@@ -52,8 +84,8 @@ $in_type = "hidden";
 	<?php echo JHTML::_( 'form.token' ); ?>
 </form>
 
-<div>
-	<?php write_debug($_REQUEST); ?>
+<div id="express-checkin-list">
+	<div class="alert alert-success">Search for <?php echo JText::_('COM_CLUBREG_PLAYERNAME_LABEL')?></div>
 </div>
 <?php 
 
@@ -64,5 +96,6 @@ ClubregHelper::writeTabAssets($document, "regmembers",array("css"));
 ClubregHelper::writeTabAssets($document, "clubreg",array("js"));
 ClubregHelper::writeTabAssets($document, "expresscheckin",array("js"));
 ClubregHelper::writeTabAssets($document, "common",array("css"));
+ClubregHelper::writeTabAssets($document, "statslist",array('css'));
 //ClubregHelper::write_footer(); 
 ?>
