95af2eaa9a56f77baa402066d1119a3479bffc3c
Omo Agbagbara <joomla@deltastateonline.com>
2016-10-05 19:37:30 +1000

    Return html from ajax call rather than json

--- components/com_clubreg/assets/js/expresscheckin.js
+++ components/com_clubreg/assets/js/expresscheckin.js
@@ -7,9 +7,9 @@ function expresscheckinButtonRequestDef(){
 };
 expresscheckinButtonRequestDef.prototype.useResults = function(response){
 	
-	Joomla.removeMessages();
-	
-	console.log(response);
+	Joomla.removeMessages();	
+	jQuery("#express-checkin-list").removeClass('loading1');
+	jQuery("#express-checkin-list").html(response);	
 }
 
 /**
@@ -20,16 +20,27 @@ var expresscheckinButtonsRequestConfig = new expresscheckinButtonRequestDef();
 jQuery(document).ready(function(){
 	
 	jQuery('#express-checkin-div').on('click','a.btn',function(){
-		var searchValue  = jQuery('#expresscheckin-adminForm #search_value').val();
+		var searchValue  = jQuery('#expresscheckinAdminForm #search_value').val();
 		searchValue = jQuery.trim(searchValue);
 		if(searchValue ){		
-			jQuery('#expresscheckin-adminForm #express-checkin-div').removeClass('error');
-			jQuery('#expresscheckin-adminForm #search_value').val(searchValue);
-			expresscheckinButtonsRequestConfig.rData = jQuery('#expresscheckin-adminForm').serialize();			
-			ClubRegObject.loadAjaxRequest(expresscheckinButtonsRequestConfig);
+			
+			jQuery("#express-checkin-list").html("");	
+			jQuery("#express-checkin-list").addClass('loading1');			
+			
+			jQuery('#expresscheckinAdminForm #express-checkin-div').removeClass('error');
+			jQuery('#expresscheckinAdminForm #search_value').val(searchValue);
+			expresscheckinButtonsRequestConfig.rData = jQuery('#expresscheckinAdminForm').serialize();	
+			
+			ClubRegObject.loadAjaxRequestHTML(expresscheckinButtonsRequestConfig);
 		}else{
-			jQuery('#expresscheckin-adminForm #express-checkin-div').addClass('error');
+			jQuery('#expresscheckinAdminForm #express-checkin-div').addClass('error');
 		}		
 	});
 	
 });
+/**
+ * Stats date has been changed
+ */
+function expressCheckinDateChanged(){	 
+	//ClubRegObject.getStats(statsListRequestConfig,ClubRegObject.currentProfiles);
+}
